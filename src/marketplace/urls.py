62ad53a96ca7a3a238e8692b37065616bcb757f3
ElMariachi-78 <lucas.martel@2025.icam.fr>
2024-12-12 12:15:44 +0100

    ok

--- src/marketplace/urls.py
+++ src/marketplace/urls.py
@@ -2,11 +2,14 @@
 from django.urls import path
 
 from shop.views import vitrine, create_vitrine, edit_vitrine, search_results, vitrine_detail, all_vitrines
-from marketplace.views import CreateProduct
+from marketplace.views import CreateProduct,ProductDetail
+
 app_name = "marketplace"
 urlpatterns = [
 
     path("create_product/", CreateProduct.as_view(), name="create"),
+    path("<slug:slug>/", ProductDetail.as_view(), name="detail"),
+
 
 
 ]
--- src/marketplace/views/seller.py
+++ src/marketplace/views/seller.py
@@ -1,6 +1,9 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.views.generic import DetailView
+from django.shortcuts import get_object_or_404
+
 
 from marketplace.models import Product
 
@@ -15,3 +18,17 @@ class CreateProduct(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class ProductDetail(DetailView):
+    model = Product
+    template_name = "product/product_detail.html"
+    context_object_name = "product"
+
+    def get_object(self):
+        # Obtenir l'article activé correspondant au slug
+        return get_object_or_404(Product, slug=self.kwargs.get("slug"), activate=True)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["vitrine"] = self.object.user.vitrine  # Si "vitrine" est lié à l'utilisateur
+        return context
